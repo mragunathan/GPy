8e48973d73e7e5840c56b7ab941b8bbe16a0c60f
mragunathan <mragunathan@gmail.com>
2018-12-15 23:23:47 +0800

    MTBO related changes

--- GPy/models/gp_coregionalized_regression.py
+++ GPy/models/gp_coregionalized_regression.py
@@ -6,6 +6,7 @@ from ..core import GP
 from .. import likelihoods
 from .. import kern
 from .. import util
+from paramz import ObsAr
 
 class GPCoregionalizedRegression(GP):
     """
@@ -44,3 +45,61 @@ class GPCoregionalizedRegression(GP):
         likelihood = util.multioutput.build_likelihood(Y_list,self.output_index,likelihoods_list)
 
         super(GPCoregionalizedRegression, self).__init__(X,Y,kernel,likelihood, Y_metadata={'output_index':self.output_index})
+
+    def set_XY(self, X=None, Y=None):
+        # print("set_XY: ")
+        # print("X.shape: ",X.shape)
+        # print("Y.shape: ",Y.shape)
+        """
+        Set the input / output data of the model
+        This is useful if we wish to change our existing data but maintain the same model
+
+        :param X: input observations
+        :type X: np.ndarray
+        :param Y: output observations
+        :type Y: np.ndarray
+        """
+        X_list = []
+        Y_list = []
+        for i in np.arange(Y.shape[1]):
+           X_list.append(X.copy())
+           Y_list.append(np.atleast_2d(Y[:,i]).T)
+        
+        # print("len(X_list): ",len(X_list))
+        # print("len(Y_list): ",len(Y_list))
+
+        X,Y,self.output_index = util.multioutput.build_XY(X_list,Y_list)
+        self.Y_metadata = {'output_index':self.output_index}
+
+        # print("after build_XY: ")
+        # print("X.shape: ",X.shape)
+        # print("Y.shape: ",Y.shape)
+        # print("self.output_index: ",self.output_index)
+
+        self.update_model(False)
+        if Y is not None:
+            if self.normalizer is not None:
+                self.normalizer.scale_by(Y)
+                self.Y_normalized = ObsAr(self.normalizer.normalize(Y))
+                self.Y = Y
+            else:
+                self.Y = ObsAr(Y)
+                self.Y_normalized = self.Y
+        if X is not None:
+            if self.X in self.parameters:
+                # LVM models
+                if isinstance(self.X, VariationalPosterior):
+                    assert isinstance(X, type(self.X)), "The given X must have the same type as the X in the model!"
+                    index = self.X._parent_index_
+                    self.unlink_parameter(self.X)
+                    self.X = X
+                    self.link_parameter(self.X, index=index)
+                else:
+                    index = self.X._parent_index_
+                    self.unlink_parameter(self.X)
+                    from ..core import Param
+                    self.X = Param('latent mean', X)
+                    self.link_parameter(self.X, index=index)
+            else:
+                self.X = ObsAr(X)
+        self.update_model(True)
